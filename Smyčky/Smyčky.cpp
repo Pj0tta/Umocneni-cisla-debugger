cf4c82a2fab76c15b8f9c69794acde7837b2db65
Petr Habel <petr.habel.020@pslib.cz>
2021-11-29 14:07:29 +0100

    Add project files.

---
+++ Smyčky/Smyčky.cpp
@@ -0,0 +1,18 @@
+﻿#include <stdio.h>
+
+int main()
+{
+	int cislo, nasobek;
+	long long vysledek = 1;
+	printf("Zadej cislo ");
+	scanf_s("%d",&cislo);
+
+	printf("Zadej pocet umocneni ");
+	scanf_s("%d", &nasobek);
+
+	for (int i = 1; i <= nasobek; i++) {
+		vysledek = vysledek * cislo;
+		
+	}
+	printf("%d", vysledek);
+}
